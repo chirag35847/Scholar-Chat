69916681c831cf8b82a9cad2ce184fd9e8b53aa8
Chirag Mittal <chirag.workid@gmail.com>
2022-09-04 17:39:20 +0530

    more meaningful errors to register api

--- backend/routes/oauth.js
+++ backend/routes/oauth.js
@@ -1,39 +1,43 @@
 const express = require("express");
-const User =require("../models/userModel");
+const User = require("../models/userModel");
 
 const asyncHandler = require("express-async-handler");
 
 const oauth = asyncHandler(async (req, res) => {
-    console.log(req.body)
-    if(req.body!={}){
-        try{
-            console.log('not empty')
-            const { orcid, name, password } = req.body;
-            if (!name || !orcid || !password) {
-                res.status(400);
-                throw new Error("Please enter all the fields");
-            }
-    
-            const user = await User.create({
-                name,
-                orcid,
-                password,
-            });
-            console.log(user);
-            if(!user){
-                console.log("some error");
-            }
-            res.status(201)
-            res.send('User Created Successfully');
-        }
-        catch(e){
-            res.status(400).send('Some Error Orrcured');
-        }
+  console.log(req.body);
+  if (req.body != {}) {
+    try {
+      console.log("not empty");
+      const { orcid, name, password } = req.body;
+      if (!name || !orcid || !password) {
+        res.status(401);
+        throw new Error("Please enter all the fields");
+      }
+
+      const userExists = await User.findOne({ orcid });
+
+      if (userExists) {
+        res.status(400);
+        throw new Error("User Alredy Exists");
+      }
+
+      const user = await User.create({
+        name,
+        orcid,
+        password,
+      });
+      if(!user){
+        res.status(402);
+        throw new Error("Internal Server Error");
+      }
+      res.status(201);
+      res.send("User Created Successfully");
+    } catch (e) {
+      res.status(402).send("Internal Server Error");
     }
-    else{
-        res.status(400)
-        res.send('You can do this')
-    }
-    
-  });
+  } else {
+    res.status(402);
+    res.send("Internal Server Error");
+  }
+});
 module.exports = oauth;
