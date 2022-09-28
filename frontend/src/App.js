039e46aab43bd3e4fbe85bfc76f97b320a9964c2
kuchiPie <shreyasbulbule007@gmail.com>
2022-09-29 00:36:10 +0530

    removed oauth api and added a func

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import ChatPage from "./pages/ChatPage";
 import HomePage from "./pages/HomePage";
 import "./App.css";
 import VerifyPage from "./pages/VerifyPage";
+import Hello from "./pages/hello";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/verify" component={VerifyPage} exact></Route>
       <Route path="/" component={HomePage} exact></Route>
       <Route path="/chats" component={ChatPage} exact></Route>
+      <Route path="/hello" component={Hello}></Route>
     </div>
   );
 }
