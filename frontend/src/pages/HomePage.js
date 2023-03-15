623dea97c089796ae8c60f8b632d8577a1d5fe66
pratikpakhale <pratikpakhale20@gmail.com>
2023-03-15 18:29:02 +0530

    fix: changed /hello -> /app route and fixed the all catch route

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react'
 import {
   Container,
   Box,
@@ -9,68 +9,64 @@ import {
   TabPanel,
   Tabs,
   Button,
-} from "@chakra-ui/react";
-import Login from "../components/Authentication/Login";
-import { Link, Redirect, useHistory } from "react-router-dom";
-
+} from '@chakra-ui/react'
+import Login from '../components/Authentication/Login'
+import { Link, Redirect, useHistory } from 'react-router-dom'
 
 const HomePage = () => {
-  const history = useHistory();
+  const history = useHistory()
   useEffect(() => {
-    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'))
     if (userInfo) {
-      history.push("/chats");
+      history.push('/chats')
     }
-  }, [history]);
+  }, [history])
 
-  const ClickHandler=()=>{
-    // const url = 'https://kuchipie.github.io/orcid-registration-frontend/';
-    // window.open(url, '_blank');
-    // return <Redirect to='/hello'/>
+  const ClickHandler = () => {
     const history = useHistory()
     function sleep(ms) {
-      return new Promise((resolve) => setTimeout(resolve, ms));
+      return new Promise(resolve => setTimeout(resolve, ms))
     }
     async function demo() {
-      await sleep(2 * 1000);
-      console.log("done");
-      window.location.reload();
+      await sleep(2 * 1000)
+      console.log('done')
+      window.location.reload()
     }
-    demo();
+    demo()
     history.go(0)
 
     window.location.reload()
   }
 
   return (
-    <Container maxW="xl" centerContent>
+    <Container maxW='xl' centerContent>
       <Box
-        d="flex"
-        justifyContent="center"
+        d='flex'
+        justifyContent='center'
         p={3}
-        bg={"white"}
-        w="100%"
-        m="40px 0 15px 0"
-        borderRadius="1g"
-        borderWidth="1px"
+        bg={'white'}
+        w='100%'
+        m='40px 0 15px 0'
+        borderRadius='1g'
+        borderWidth='1px'
       >
-        <Text fontSize="4xl" fontFamily="Work sans" color="black">
+        <Text fontSize='4xl' fontFamily='Work sans' color='black'>
           Scholar Chat
         </Text>
       </Box>
 
       <Box
-        bg="white"
-        w="100%"
+        bg='white'
+        w='100%'
         p={4}
-        borderRadius="1g"
-        color="black"
-        borderWidth="1px"
+        borderRadius='1g'
+        color='black'
+        borderWidth='1px'
       >
-        <Tabs variant="soft-rounded">
-          <TabList mb={"1em"}>
-            <Tab width={"50%"}>Login</Tab>
-            <Tab width={"50%"}>Register</Tab>
+        <Tabs variant='soft-rounded'>
+          <TabList mb={'1em'}>
+            <Tab width={'50%'}>Login</Tab>
+            <Tab width={'50%'}>Register</Tab>
           </TabList>
 
           <TabPanels>
@@ -78,16 +74,23 @@ const HomePage = () => {
               <Login />
             </TabPanel>
             <TabPanel>
-              <div style={{display:"flex",justifyContent:'center',alignItems:'center'}}>
-                <Link to='/hello'><Button >Register With Orcid</Button></Link>
-                
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Link to='/app'>
+                  <Button>Register With Orcid</Button>
+                </Link>
               </div>
             </TabPanel>
           </TabPanels>
         </Tabs>
       </Box>
     </Container>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
